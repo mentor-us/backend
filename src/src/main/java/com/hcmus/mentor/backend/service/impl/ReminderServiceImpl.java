e71d81d86fc3deb6cee99b1626a938e830994126
hieucckha <hieucckha@gmail.com>
2024-02-12 19:32:50 +0700

    Revert ReminderService MU-179

--- src/src/main/java/com/hcmus/mentor/backend/service/impl/ReminderServiceImpl.java
+++ src/src/main/java/com/hcmus/mentor/backend/service/impl/ReminderServiceImpl.java
@@ -1,6 +1,10 @@
 package com.hcmus.mentor.backend.service.impl;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.hcmus.mentor.backend.domain.Group;
+import com.hcmus.mentor.backend.domain.Reminder;
+import com.hcmus.mentor.backend.domain.User;
+import com.hcmus.mentor.backend.domain.constant.ReminderType;
 import com.hcmus.mentor.backend.repository.GroupRepository;
 import com.hcmus.mentor.backend.repository.ReminderRepository;
 import com.hcmus.mentor.backend.repository.UserRepository;
@@ -10,6 +14,8 @@ import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,60 +27,36 @@ public class ReminderServiceImpl implements ReminderService {
     private final GroupRepository groupRepository;
     private final MailService mailService;
 
-    //    public List<Reminder> getReminders(IRemindableService remindableService){
-    //        return remindableService.findReminderToday();
-    //    }
-
-    //    public List<Reminder> saveReminders(){
-    //        List<Reminder> reminders = new ArrayList<>();
-    //        List<Reminder> meetingReminders = getReminders(meetingService);
-    //        List<Reminder> taskReminders = getReminders(taskService);
-    //
-    //        reminders.addAll(meetingReminders);
-    //        reminders.addAll(taskReminders);
-    //
-    //        reminderRepository.saveAll(reminders);
-    //
-    //        return reminders;
-    //    }
-
     @Override
     public void sendReminders() throws FirebaseMessagingException {
-        System.out.println("Send reminders");
-//        List<Reminder> reminders = reminderRepository.findByReminderDateBefore(new Date());
-//
-//        for (Reminder reminder : reminders) {
-//            String template = reminder.getType().toString() + "_REMINDER";
-//            String subject = reminder.getSubject();
-//            Map<String, Object> properties = reminder.getProperties();
-//            List<String> recipients = reminder.getRecipients();
-//            List<String> receiverIds = new ArrayList<>();
-//            recipients.forEach(recipient -> {
-//                mailService.sendEmailTemplate(template, properties, subject, Collections.singletonList(recipient));
-//                logger.info("Send reminder {} to {}", reminder.getType(), recipient);
-//
-//                Optional<User> userOptional = userRepository.findByEmail(recipient);
-//                userOptional.ifPresent(user -> receiverIds.add(user.getId()));
-//            });
-//            Optional<Group> groupOptional = groupRepository.findById(reminder.getGroupId());
-//            String title = groupOptional.isPresent() ? groupOptional.get().getName() : "MentorUS";
-//            String body = "";
-//            if (reminder.getType() == ReminderType.MEETING) {
-//                body = "Lịch hẹn "
-//                        + reminder.getName()
-//                        + " sẽ diễn ra lúc "
-//                        + reminder.getProperties().get("dueDate");
-//            } else if (reminder.getType() == ReminderType.TASK) {
-//                body = "Công việc "
-//                        + reminder.getName()
-//                        + " sẽ tới hạn lúc "
-//                        + reminder.getProperties().get("dueDate");
-//            }
-//            firebaseMessagingManager.sendGroupNotification(receiverIds, title, body);
-//        }
-//
-//        if (!reminders.isEmpty()) {
-//            reminderRepository.deleteAll(reminders);
-//        }
+        List<Reminder> reminders = reminderRepository.findByReminderDateBefore(new Date());
+
+        for (Reminder reminder : reminders) {
+            String template = reminder.getType().toString() + "_REMINDER";
+            String subject = reminder.getSubject();
+            Map<String, Object> properties = reminder.getProperties();
+            List<String> recipients = reminder.getRecipients();
+            List<String> receiverIds = new ArrayList<>();
+            recipients.forEach(recipient -> {
+                mailService.sendEmailTemplate(template, properties, subject, Collections.singletonList(recipient));
+                logger.info("Send reminder {} to {}", reminder.getType(), recipient);
+
+                Optional<User> userOptional = userRepository.findByEmail(recipient);
+                userOptional.ifPresent(user -> receiverIds.add(user.getId()));
+            });
+            Optional<Group> groupOptional = groupRepository.findById(reminder.getGroupId());
+            String title = groupOptional.isPresent() ? groupOptional.get().getName() : "MentorUS";
+            String body = "";
+            if (reminder.getType() == ReminderType.MEETING) {
+                body = String.format("Lịch hẹn %s sẽ diễn ra lúc %s", reminder.getName(), reminder.getProperties().get("dueDate"));
+            } else if (reminder.getType() == ReminderType.TASK) {
+                body = String.format("Công việc %s sẽ tới hạn lúc %s", reminder.getName(), reminder.getProperties().get("dueDate"));
+            }
+            firebaseMessagingManager.sendGroupNotification(receiverIds, title, body);
+        }
+
+        if (!reminders.isEmpty()) {
+            reminderRepository.deleteAll(reminders);
+        }
     }
 }
