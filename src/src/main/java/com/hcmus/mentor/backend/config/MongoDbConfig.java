b1182486685497c021bc1b3a3eff6b4e2f8cd7d9
hieucckha <hieucckha@gmail.com>
2024-02-12 19:19:22 +0700

    Add pooling option for mongo client MU-209

---
+++ src/src/main/java/com/hcmus/mentor/backend/config/MongoDbConfig.java
@@ -0,0 +1,26 @@
+package com.hcmus.mentor.backend.config;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.connection.ConnectionPoolSettings;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
+
+public class MongoDbConfig {
+
+    @Bean
+    public MongoClientSettings mongoClientSettings() {
+        return MongoClientSettings.builder()
+                .retryWrites(true)
+                .applyToConnectionPoolSettings((ConnectionPoolSettings.Builder builder) -> builder
+                        .maxSize(300) //connections count
+                        .minSize(100)
+                        .maxConnectionLifeTime(0, TimeUnit.MILLISECONDS)
+                        .maxConnectionIdleTime(0, TimeUnit.MILLISECONDS)
+                        .maxWaitTime(5000, TimeUnit.MILLISECONDS))
+                .applyToSocketSettings(builder -> builder
+                        .connectTimeout(2000, TimeUnit.MILLISECONDS))
+                .applicationName("app")
+                .build();
+    }
+}
