04261659719743425919f206386e323084706404
capybbv <vothanhsuong123@gmail.com>
2024-06-01 16:26:04 +0700

    Add jackson for java8 data type MU-333

---
+++ src/src/main/java/com/hcmus/mentor/backend/config/JsonConfig.java
@@ -0,0 +1,27 @@
+package com.hcmus.mentor.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    public ObjectMapper getJacksonObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new ParameterNamesModule());
+        objectMapper.registerModule(new Hibernate6Module());
+
+        objectMapper.configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        return objectMapper;
+    }
+}
