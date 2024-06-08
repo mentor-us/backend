a6e51fc3845d551108f4aa70ca8c4df1ad5ee043
hieucckha <hieucckha@gmail.com>
2024-06-08 14:59:42 +0700

    Add blackbird jackson

--- src/src/main/java/com/hcmus/mentor/backend/config/JsonConfig.java
+++ src/src/main/java/com/hcmus/mentor/backend/config/JsonConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.blackbird.BlackbirdModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +22,7 @@ public class JsonConfig {
         objectMapper.registerModule(new Jdk8Module());
         objectMapper.registerModule(new ParameterNamesModule());
         objectMapper.registerModule(new Hibernate6Module());
+        objectMapper.registerModule(new BlackbirdModule());
 
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 
