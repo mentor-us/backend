52ea187695dbf6508a2668023eea59d1300f5dd2
capybbv <vothanhsuong123@gmail.com>
2024-06-18 08:31:02 +0700

    Fix query get list user has note by viewer MU-402

---
+++ src/src/main/java/com/hcmus/mentor/backend/controller/usecase/note/searchuserhasnotebyviewer/SearchUserHasNoteByViewerQueryHandler.java
@@ -0,0 +1,26 @@
+package com.hcmus.mentor.backend.controller.usecase.note.searchuserhasnotebyviewer;
+
+import an.awesome.pipelinr.Command;
+import com.hcmus.mentor.backend.repository.UserRepository;
+import com.hcmus.mentor.backend.security.principal.LoggedUserAccessor;
+import com.hcmus.mentor.backend.util.MappingUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SearchUserHasNoteByViewerQueryHandler implements Command.Handler<SearchUserHasNoteByViewerQuery, SearchUserHasNoteByViewerResult> {
+    private final UserRepository userRepository;
+    private final LoggedUserAccessor loggedUserAccessor;
+
+    @Override
+    public SearchUserHasNoteByViewerResult handle(SearchUserHasNoteByViewerQuery command) {
+        var currentUserId = loggedUserAccessor.getCurrentUserId();
+        var noteUserProfilePage = userRepository.findAllAccessNote(currentUserId, PageRequest.of(command.getPage(), command.getPageSize()));
+        var result = new SearchUserHasNoteByViewerResult();
+        result.setData(noteUserProfilePage.getContent());
+        MappingUtil.mapPageQueryMetadata(noteUserProfilePage, result);
+        return result;
+    }
+}
---
+++ src/src/main/java/com/hcmus/mentor/backend/controller/usecase/note/searchuserhasnotebyviewer/SearchUserHasNoteByViewerResult.java
@@ -0,0 +1,7 @@
+package com.hcmus.mentor.backend.controller.usecase.note.searchuserhasnotebyviewer;
+
+import com.hcmus.mentor.backend.controller.usecase.common.pagination.PageQueryResult;
+import com.hcmus.mentor.backend.controller.usecase.note.common.NoteUserProfile;
+
+public class SearchUserHasNoteByViewerResult extends PageQueryResult<NoteUserProfile> {
+}
