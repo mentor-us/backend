40ea52e428c7f1cc9112ab73a27344a1571ff8e0
hieucckha <hieucckha@gmail.com>
2024-07-27 22:15:36 +0700

    Add search for auditlog

--- src/src/main/java/com/hcmus/mentor/backend/controller/usecase/auditrecord/search/SearchAuditRecordQuery.java
+++ src/src/main/java/com/hcmus/mentor/backend/controller/usecase/auditrecord/search/SearchAuditRecordQuery.java
@@ -2,14 +2,25 @@ package com.hcmus.mentor.backend.controller.usecase.auditrecord.search;
 
 import an.awesome.pipelinr.Command;
 import com.hcmus.mentor.backend.controller.usecase.common.pagination.PageQueryFilter;
+import com.hcmus.mentor.backend.domain.constant.ActionType;
+import com.hcmus.mentor.backend.domain.constant.DomainType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 public class SearchAuditRecordQuery extends PageQueryFilter implements Command<SearchAuditRecordResult> {
+
+    private String userName;
+    private String userEmail;
+    private ActionType action;
+    private DomainType domain;
+    private LocalDateTime from;
+    private LocalDateTime to;
 }
