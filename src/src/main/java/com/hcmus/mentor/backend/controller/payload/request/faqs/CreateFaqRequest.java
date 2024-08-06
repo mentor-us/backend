7be7d7e983e55d549b7097650c73d8369477797d
hieucckha <hieucckha@gmail.com>
2024-08-06 08:09:56 +0700

    Add validation for answer faq 0051818

--- src/src/main/java/com/hcmus/mentor/backend/controller/payload/request/faqs/CreateFaqRequest.java
+++ src/src/main/java/com/hcmus/mentor/backend/controller/payload/request/faqs/CreateFaqRequest.java
@@ -15,6 +15,7 @@ public class CreateFaqRequest {
     @Length(min = 1, max = 255, message ="Câu hỏi không vượt quá 255 ký tự.")
     private String question;
 
+    @Length(min = 1, max = 1000, message ="Câu trả lời không vượt quá 1000 ký tự.")
     private String answer;
 
     private String groupId;
