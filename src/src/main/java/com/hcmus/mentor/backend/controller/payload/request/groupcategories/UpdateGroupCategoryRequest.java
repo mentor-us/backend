e547c9d94f33ac7203eb91cad32522f7533fba2b
hieucckha <hieucckha@gmail.com>
2024-08-10 11:01:01 +0700

    Remove the required for description group category

--- src/src/main/java/com/hcmus/mentor/backend/controller/payload/request/groupcategories/UpdateGroupCategoryRequest.java
+++ src/src/main/java/com/hcmus/mentor/backend/controller/payload/request/groupcategories/UpdateGroupCategoryRequest.java
@@ -20,7 +20,7 @@ public class UpdateGroupCategoryRequest {
     @Size(min = 1, max = 100, message = "Tên loại nhóm không được không quá 100 ký tự")
     private String name;
 
-    @Size(min = 1, max = 200, message = "Mô tả không được không quá 200 ký tự")
+    @Size(max = 200, message = "Mô tả không được không quá 200 ký tự")
     private String description;
 
     private Boolean status;
